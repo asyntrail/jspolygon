a6de183572383747de5fdb702d89c57379d1a3a7
asyntrail <asyntrail@gmail.com>
2017-08-24 20:16:26 +0300

    test loop

---
+++ js/polygon.js
@@ -0,0 +1,14 @@
+// var mystring1 = "hello".length;
+// document.write(mystring1);
+
+var i=0;
+while (i<=20) {
+    document.write(i + "<br />");
+    i+=2;
+}
+
+/* var i = 0;
+for (; i <= 10; ) {
+    document.write(i+ "<br />");
+    i+=2;
+}*/
