e13d07e31fdd60021996fe619716c17835ecbd84
asyntrail <asyntrail@gmail.com>
2018-05-01 20:33:11 +0300

    Added check on enter,focus

--- js/polygon.js
+++ js/polygon.js
@@ -1,38 +1,52 @@
 function count_f() {
 
 
-function randomRange(myMin, myMax) {
-      return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    function randomRange(myMin, myMax) {
+        return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    }
+
+
+
+    var myRandom = randomRange(2, 9);
+    var secondRandom = randomRange(2, 9);
+    var a = myRandom + '*' + secondRandom;
+    var c = "";
+    document.getElementById('target').innerHTML = a;
+    document.getElementById('result').innerHTML = c;
+    document.getElementById('expression').value = "";
+    expression.className = "result2";
+    next.innerHTML = "Next"
+    document.getElementById("expression").focus();
+
+
+
+    function check() {
+
+        var b = myRandom * secondRandom;
+
+        var expr = document.getElementById('expression').value;
+        if (expr == b) {
+            expression.className = "result";
+            result.innerHTML = 'Yes, result is: ' + b
+        } else {
+            expression.className = "result1";
+            result.innerHTML = 'No, result is: ' + b
+
+        }
+    };
+
+
+    document.getElementById("expression")
+        .addEventListener("keyup", function(event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                check();
+
+
+            }
+        });
+
 }
 
-var myRandom = randomRange(0, 100);
-var secondRandom = randomRange(0, 100);
-var a = myRandom+'+'+secondRandom;
-var c="";
-var expr="aa";
-document.getElementById('target').innerHTML = a;
-document.getElementById('result').innerHTML = c;
-document.getElementById('expression').value = "";
-expression.className  = "result2";
-next.innerHTML = "Next";
-
-
-expression.onblur = function() {
-  var b=myRandom+secondRandom;
-
-  var expr = document.getElementById('expression').value;
-  if (expr==b) {
-   expression.className  = "result";
-    result.innerHTML = 'Yes, result is: '+b
-  }
-  else{
-    this.className = "result1";
-    result.innerHTML = 'No, result is: '+b
-
-  }
-};
-
-
-}
 
 document.getElementById('next').addEventListener('click', count_f);
