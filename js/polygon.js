ae8d377add572e03aa603cf2ec7a1b60171a8c1c
asyntrail <asyntrail@gmail.com>
2020-09-18 17:06:47 +0300

    add new parametrs

--- js/polygon.js
+++ js/polygon.js
@@ -1,48 +1,82 @@
+
+window.onload = function(){
+var sc =0;
+
+
+
 function count_f() {
-
-
+document.getElementById('score').innerHTML =sc;
+//функция генерации рандомных чмсел
     function randomRange(myMin, myMax) {
         return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
     }
+var arr = [1, 10];
+var arr2 = [1, 100];
+var arr3 = [1, 1000];
+var [r1,r2] = arr;
+var [r3,r4] = arr2;
+var [r5,r6] = arr3;
 
 
 
-    var myRandom = randomRange(2, 9);
-    var secondRandom = randomRange(2, 9);
-    var a = myRandom + '*' + secondRandom;
-    var c = "";
-    document.getElementById('target').innerHTML = a;
-    document.getElementById('result').innerHTML = c;
+    if (document.getElementById('one').checked){
+     myRandom = randomRange(r1, r2);
+     secondRandom = randomRange(r1, r2);
+    } else if  (document.getElementById('ten').checked){
+     myRandom = randomRange(r3, r4);
+     secondRandom = randomRange(r3, r4);
+    } else if  (document.getElementById('hundreds').checked){
+     myRandom = randomRange(r5, r6);
+     secondRandom = randomRange(r5, r6);
+    }
+
+
+    //document.getElementById('hint').innerHTML = "";
     document.getElementById('expression').value = "";
-    expression.className = "result2";
-    next.innerHTML = "Next"
+   expression.className = "result2";
+     next.innerHTML = "Next";
     document.getElementById("expression").focus();
 
 
 
+    if (document.getElementById('plus').checked){
+         a = myRandom + '+' + secondRandom;
+         b = myRandom + secondRandom;
+    } else if (document.getElementById('minus').checked){
+        a = myRandom + '-' + secondRandom;
+        b = myRandom - secondRandom;
+    } else if (document.getElementById('multiple').checked){
+        a = myRandom + '*' + secondRandom;
+        b = myRandom * secondRandom;
+    }
+
+    document.getElementById('target').innerHTML = a;
+    document.getElementById('hint').innerHTML =" Press enter to check";
+//фунцкия проверки выражения
     function check() {
-
-        var b = myRandom * secondRandom;
-
         var expr = document.getElementById('expression').value;
         if (expr == b) {
             expression.className = "result";
-            result.innerHTML = 'Yes, result is: ' + b
-        } else {
+            hint.innerHTML = 'Yes, result is: ' + b;
+            sc = sc+1;
+            document.getElementById('score').innerHTML =sc;
+
+        } else if (expr === ""){
+           alert("Enter the value");
+        }
+        else {
             expression.className = "result1";
-            result.innerHTML = 'No, result is: ' + b
+            hint.innerHTML = 'No, result is: ' + b;
 
         }
-    };
+    }
 
-
+//блок кода запускающий функцию проверки по клавише
     document.getElementById("expression")
         .addEventListener("keyup", function(event) {
             event.preventDefault();
             if (event.keyCode === 13) {
                 check();
-
-
             }
         });
 
@@ -50,3 +84,7 @@ function count_f() {
 
 
 document.getElementById('next').addEventListener('click', count_f);
+
+
+};
+
